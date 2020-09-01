ee18ac67e6171436efb828749eef303192e662eb
Christian Schwang <cschwang@bhs-world.com>
2020-09-01 22:09:41 +0800

    Factor Out Context Menu Creation into separate method

--- MyCustomApplicationContext.cs
+++ MyCustomApplicationContext.cs
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using MouseUnSnag.CommandLine;
 using MouseUnSnag.Properties;
@@ -13,55 +15,64 @@ namespace MouseUnSnag
     {
         private readonly NotifyIcon _trayIcon;
 
+        /// <summary>
+        /// This is the one that runs "forever" while the application is alive, and handles
+        /// events, etc. This application is ABSOLUTELY ENTIRELY driven by the LLMouseHook
+        /// and DisplaySettingsChanged events.
+        /// </summary>
+        /// <param name="options"></param>
         public MyCustomApplicationContext(Options options)
         {
             _trayIcon = new NotifyIcon
             {
                 Icon = Resources.Icon,
-                ContextMenu = new ContextMenu
-                {
-                    MenuItems =
-                    {
-                        new MenuItem("UnStick from corners", (sender, _) =>
-                        {
-                            var item = (MenuItem) sender;
-                            options.Unstick = !options.Unstick;
-                            item.Checked = options.Unstick;
-                        })
-                        {
-                            Checked = options.Unstick
-                        },
-
-                        new MenuItem("Jump between monitors", (sender, _) =>
-                        {
-                            var item = (MenuItem) sender;
-                            options.Jump = !options.Jump;
-                            item.Checked = options.Jump;
-                        })
-                        {
-                            Checked = options.Jump
-                        },
-
-                        new MenuItem("Wrap around monitors", (sender, _) =>
-                        {
-                            var item = (MenuItem) sender;
-                            options.Wrap = !options.Wrap;
-                            item.Checked = options.Wrap;
-                        })
-                        {
-                            Checked = options.Wrap
-                        },
-
-                        new MenuItem("Exit", delegate
-                        {
-                            _trayIcon.Visible = false;
-                            Application.Exit();
-                        })
-                    }
-                },
+                ContextMenu = new ContextMenu(MakeMenuItems(options).ToArray()),
                 Visible = true
             };
         }
+
+        private IEnumerable<MenuItem> MakeMenuItems(Options options)
+        {
+            return new[]
+            {
+                new MenuItem("UnStick from corners", (sender, _) =>
+                {
+                    var item = (MenuItem) sender;
+                    options.Unstick = !options.Unstick;
+                    item.Checked = options.Unstick;
+                })
+                {
+                    Checked = options.Unstick
+                },
+
+                new MenuItem("Jump between monitors", (sender, _) =>
+                {
+                    var item = (MenuItem) sender;
+                    options.Jump = !options.Jump;
+                    item.Checked = options.Jump;
+                })
+                {
+                    Checked = options.Jump
+                },
+
+                new MenuItem("Wrap around monitors", (sender, _) =>
+                {
+                    var item = (MenuItem) sender;
+                    options.Wrap = !options.Wrap;
+                    item.Checked = options.Wrap;
+                })
+                {
+                    Checked = options.Wrap
+                },
+
+                new MenuItem("Exit", delegate
+                {
+                    _trayIcon.Visible = false;
+                    Application.Exit();
+                })
+            };
+        }
+        
 
     }
 }
