83fda4ca84a7b7aea25982c1de43b54cead038d1
Christian Schwang <cschwang@bhs-world.com>
2020-09-05 17:53:34 +0800

    Basic Unit test for Display

---
+++ MouseUnSnagTests/ScreenHandling/DisplayTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MouseUnSnag.ScreenHandling;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseUnSnag.ScreenHandling.Tests
+{
+    [TestClass()]
+    public class DisplayTests
+    {
+        [TestMethod()]
+        public void DisplayTest()
+        {
+            var screen = new VirtualScreen(99, 
+                new Rectangle(9999, 9999, 9999, 9999), 
+                "Awesome Screen", 
+                false,
+                new Rectangle(8888, 8888, 8888, 888));
+
+            var screenNumber = 12;
+
+            var d = new Display(screen, screenNumber);
+            
+            
+            // Null Checks
+            Assert.IsNotNull(d.Above);
+            Assert.IsNotNull(d.Below);
+            Assert.IsNotNull(d.ToLeft);
+            Assert.IsNotNull(d.ToRight);
+            Assert.IsNotNull(d.ToString());
+            Assert.IsNotNull(d.DetailledDescription());
+
+            // Screen Stuff
+            Assert.AreEqual(screenNumber, d.ScreenNumber);
+            Assert.AreSame(screen, d.Screen);
+            Assert.AreEqual(screen.Bounds, d.Bounds);
+
+            Console.WriteLine(d.DetailledDescription());
+            
+        }
+
+
+    }
+}
