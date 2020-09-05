7a554a704a951b6a25c87965c356d0c2ffe552a5
Christian Schwang <cschwang@bhs-world.com>
2020-09-05 19:22:52 +0800

    Raise events when config changes

--- MouseUnSnag/CommandLine/Options.cs
+++ MouseUnSnag/CommandLine/Options.cs
@@ -1,13 +1,73 @@
-﻿namespace MouseUnSnag.CommandLine
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MouseUnSnag.CommandLine
 {
-    class Options
+    public class Options
     {
-        public bool Unstick { get; set; } = true;
+        private bool _unstick = true;
+        private bool _jump = true;
+        private bool _wrap = true;
+        private bool _rescale = false;
 
-        public bool Jump { get; set; } = true;
+        public bool Unstick
+        {
+            get => _unstick;
+            set
+            {
+                if (_unstick == value)
+                    return;
+                _unstick = value;
+                OnConfigChanged();
+            }
+        }
 
-        public bool Wrap { get; set; } = true;
+        public bool Jump
+        {
+            get => _jump;
+            set
+            {
+                if (_jump == value)
+                    return;
+                _jump = value;
+                OnConfigChanged();
+            }
+        }
 
-        public bool Rescale { get; set; } = false;
+        public bool Wrap
+        {
+            get => _wrap;
+            set
+            {
+                if (_wrap == value)
+                    return;
+                
+                _wrap = value;
+                OnConfigChanged();
+            }
+        }
+
+        public bool Rescale
+        {
+            get => _rescale;
+            set
+            {
+                if (_rescale == value)
+                    return;
+                
+                _rescale = value;
+                OnConfigChanged();
+            }
+        }
+
+
+        public event EventHandler ConfigChanged;
+        protected virtual void OnConfigChanged()
+        {
+            EventHandler handler = ConfigChanged;
+            handler?.Invoke(this, EventArgs.Empty);
+        }
+
     }
 }
---
+++ MouseUnSnagTests/CommandLine/OptionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MouseUnSnag.CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseUnSnag.CommandLine.Tests
+{
+    [TestClass()]
+    public class OptionsTests
+    {
+        [TestMethod()]
+        public void EventTest()
+        {
+            var options = new Options();
+            var events = 0;
+
+            options.ConfigChanged += (sender, args) => { events += 1; };
+
+            options.Jump = !options.Jump;
+            Assert.AreEqual(1, events);
+
+            options.Rescale = !options.Rescale;
+            Assert.AreEqual(2, events);
+
+            options.Unstick = !options.Unstick;
+            Assert.AreEqual(3, events);
+
+            options.Wrap = !options.Wrap;
+            Assert.AreEqual(4, events);
+
+        }
+    }
+}
