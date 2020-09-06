f3277dc318e3023b6ece7fbf4a5364c36620f7dc
Christian Schwang <cschwang@bhs-world.com>
2020-09-06 16:38:43 +0800

    Apply Code style (method parameters --> first char == lowercase)

--- MouseUnSnag/GeometryUtil.cs
+++ MouseUnSnag/GeometryUtil.cs
@@ -24,10 +24,10 @@ namespace MouseUnSnag
         /// "Direction" vector from P1 to P2. X/Y of returned point will have values
         /// of -1, 0, or 1 only (vector is not normalized to length 1).
         /// </summary>
-        /// <param name="P1"><see cref="Point"/></param>
-        /// <param name="P2"><see cref="Point"/></param>
+        /// <param name="p1"><see cref="Point"/></param>
+        /// <param name="p2"><see cref="Point"/></param>
         /// <returns><see cref="Point"/> with X, Y having values corresponding to their sign</returns>
-        public static Point Direction(Point P1, Point P2) => Sign(P2 - (Size)P1);
+        public static Point Direction(Point p1, Point p2) => Sign(p2 - (Size)p1);
 
 
         /// <summary>
@@ -35,30 +35,30 @@ namespace MouseUnSnag
         /// Otherwise, it returns the (x,y) delta (sign is preserved) from P to the
         /// nearest edge/corner of R. 
         /// </summary>
-        /// <param name="R"><see cref="Rectangle"/></param>
-        /// <param name="P"><see cref="Point"/></param>
+        /// <param name="r"><see cref="Rectangle"/></param>
+        /// <param name="p"><see cref="Point"/></param>
         /// <returns>Distance</returns>
-        public static int OutsideXDistance(Rectangle R, Point P) => Math.Max(Math.Min(0, P.X - R.Left), P.X - R.Right + 1); // For Right we must correct by 1, since the Rectangle Right is one larger than the largest valid pixel
+        public static int OutsideXDistance(Rectangle r, Point p) => Math.Max(Math.Min(0, p.X - r.Left), p.X - r.Right + 1); // For Right we must correct by 1, since the Rectangle Right is one larger than the largest valid pixel
 
         /// <summary>
         /// If P is anywhere inside R, then OutsideDistance() returns (0,0).
         /// Otherwise, it returns the (x,y) delta (sign is preserved) from P to the
         /// nearest edge/corner of R. 
         /// </summary>
-        /// <param name="R"><see cref="Rectangle"/></param>
-        /// <param name="P"><see cref="Point"/></param>
+        /// <param name="r"><see cref="Rectangle"/></param>
+        /// <param name="p"><see cref="Point"/></param>
         /// <returns>Distance</returns>
-        public static int OutsideYDistance(Rectangle R, Point P) => Math.Max(Math.Min(0, P.Y - R.Top), P.Y - R.Bottom + 1); // For Bottom we must correct by 1, since the Rectangle Bottom is one larger than the largest valid pixel
+        public static int OutsideYDistance(Rectangle r, Point p) => Math.Max(Math.Min(0, p.Y - r.Top), p.Y - r.Bottom + 1); // For Bottom we must correct by 1, since the Rectangle Bottom is one larger than the largest valid pixel
 
         /// <summary>
         /// If P is anywhere inside R, then OutsideDistance() returns (0,0).
         /// Otherwise, it returns the (x,y) delta (sign is preserved) from P to the
         /// nearest edge/corner of R. 
         /// </summary>
-        /// <param name="R"><see cref="Rectangle"/></param>
-        /// <param name="P"><see cref="Point"/></param>
+        /// <param name="r"><see cref="Rectangle"/></param>
+        /// <param name="p"><see cref="Point"/></param>
         /// <returns><see cref="Point"/> where X and Y represents the distance from the rectangle</returns>
-        public static Point OutsideDistance(Rectangle R, Point P) => new Point(OutsideXDistance(R, P), OutsideYDistance(R, P));
+        public static Point OutsideDistance(Rectangle r, Point p) => new Point(OutsideXDistance(r, p), OutsideYDistance(r, p));
 
         
         /// <summary>
@@ -68,40 +68,40 @@ namespace MouseUnSnag
         /// rectangle boundary to the Point. If Point is inside Rectangle, then it just returns
         /// the point.
         /// </summary>
-        /// <param name="R"><see cref="Rectangle"/></param>
-        /// <param name="P"><see cref="Point"/></param>
+        /// <param name="r"><see cref="Rectangle"/></param>
+        /// <param name="p"><see cref="Point"/></param>
         /// <returns><see cref="Point"/></returns>
-        public static Point ClosestBoundaryPoint(this Rectangle R, Point P)
+        public static Point ClosestBoundaryPoint(this Rectangle r, Point p)
             => new Point(
-                Math.Max(Math.Min(P.X, R.Right - 1), R.Left),
-                Math.Max(Math.Min(P.Y, R.Bottom - 1), R.Top));
+                Math.Max(Math.Min(p.X, r.Right - 1), r.Left),
+                Math.Max(Math.Min(p.Y, r.Bottom - 1), r.Top));
 
         /// <summary>
         /// In which direction(s) is(are) the point outside of the rectangle? If P is
         /// inside R, then this returns (0,0). Else X and/or Y can be either -1 or
         /// +1, depending on which direction P is outside R.
         /// </summary>
-        /// <param name="R"><see cref="Rectangle"/></param>
-        /// <param name="P"><see cref="Point"/></param>
+        /// <param name="r"><see cref="Rectangle"/></param>
+        /// <param name="p"><see cref="Point"/></param>
         /// <returns><see cref="Point"/></returns>
-        public static Point OutsideDirection(Rectangle R, Point P) => Sign(OutsideDistance(R, P));
+        public static Point OutsideDirection(Rectangle r, Point p) => Sign(OutsideDistance(r, p));
 
 
         /// <summary>
         /// Check if both Rectangles overlap in the X Direction
         /// </summary>
-        /// <param name="R1"><see cref="Rectangle"/></param>
-        /// <param name="R2"><see cref="Rectangle"/></param>
+        /// <param name="r1"><see cref="Rectangle"/></param>
+        /// <param name="r2"><see cref="Rectangle"/></param>
         /// <returns></returns>
-        public static bool OverlapX(Rectangle R1, Rectangle R2) => (R1.Left < R2.Right) && (R1.Right > R2.Left);
+        public static bool OverlapX(Rectangle r1, Rectangle r2) => (r1.Left < r2.Right) && (r1.Right > r2.Left);
 
         /// <summary>
         /// Check if both Rectangles overlap in the Y Direction
         /// </summary>
-        /// <param name="R1"><see cref="Rectangle"/></param>
-        /// <param name="R2"><see cref="Rectangle"/></param>
+        /// <param name="r1"><see cref="Rectangle"/></param>
+        /// <param name="r2"><see cref="Rectangle"/></param>
         /// <returns></returns>
-        public static bool OverlapY(Rectangle R1, Rectangle R2) => (R1.Top < R2.Bottom) && (R1.Bottom > R2.Top);
+        public static bool OverlapY(Rectangle r1, Rectangle r2) => (r1.Top < r2.Bottom) && (r1.Bottom > r2.Top);
 
 
         /// <summary>
